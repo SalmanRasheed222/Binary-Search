137e8c87d908c10e915748126e5c15e59a815aba
SALMAN <SALMAN@DESKTOP-Q0BOMET>
2022-09-11 23:51:02 +0500

    Add project files.

---
+++ Binary Search/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+
+
+int[] items = new int[6];
+int search, first=0, mid,last;
+last = items.Length - 1;
+
+
+for (int i=0; i < items.Length; i++)
+{
+	Console.WriteLine("Enter Values:");
+	items[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Enter Search Value:");
+search = Convert.ToInt32(Console.ReadLine());
+
+
+
+while (first <= last)
+{
+	mid = (first + last) / 2;
+
+	if (search == items[mid])
+	{
+		Console.WriteLine("value found at:"+(mid+1));
+		return;
+	}
+
+	else if (search > items[mid])
+	{
+		first = mid + 1;
+	}
+	else
+	{
+		last = mid - 1;
+	}
+
+}
+
+
+Console.WriteLine("Value Not Found !");
+
+
+
+
+
+
+
